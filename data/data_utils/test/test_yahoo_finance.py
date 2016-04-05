3c13077438b69a11a50020ff065e40fe39719957
Thibaut Xiong <thibaut.xiong@gmail.com>
2016-04-04 22:33:26 -0500

    update yahoo_finance

--- data/data_utils/test/test_yahoo_finance.py
+++ data/data_utils/test/test_yahoo_finance.py
@@ -1,4 +1,6 @@
 import unittest
+import datetime as dt
+import data.data_utils.yahoo_finance as yf
 
 class TestYahooFinance(unittest.TestCase):
 
@@ -6,27 +8,12 @@ class TestYahooFinance(unittest.TestCase):
     def setUpClass(cls):
         cls.symbol = 'GOOG'
         cls.symbol2 = 'AAPL'
-        cls.start_date = dt(year=2016, month=1, day=1)
-        cls.end_date = dt(year=2016, month=2, day=1)
-        cls.test_date = dt(year=2016, month=2, day=1)
+        cls.start_date = dt.datetime(year=2016, month=1, day=1)
+        cls.end_date = dt.datetime(year=2016, month=2, day=1)
+        cls.test_date = dt.datetime(year=2016, month=2, day=1)
 
     def test_get_stock_data(self):
-        data = get_stock_data(self.symbol, self.start_date, self.end_date)
-        self.assertTrue(data.keys().__contains__('Open'))
-        self.assertTrue(data.keys().__contains__('High'))
-        self.assertTrue(data.keys().__contains__('Low'))
-        self.assertTrue(data.keys().__contains__('Close'))
-        self.assertTrue(data.keys().__contains__('Volume'))
-
-        data_no_start_or_end = get_stock_data(self.symbol)
-        self.assertTrue(data_no_start_or_end.keys().__contains__('Open'))
-        self.assertTrue(data_no_start_or_end.keys().__contains__('High'))
-        self.assertTrue(data_no_start_or_end.keys().__contains__('Low'))
-        self.assertTrue(data_no_start_or_end.keys().__contains__('Close'))
-        self.assertTrue(data_no_start_or_end.keys().__contains__('Volume'))
-
-    def test_get_stock_data_multiple(self):
-        data = get_stock_data([self.symbol, 'AAPL'], self.start_date, self.end_date)
+        data = yf.get_stock_data(self.symbol, self.start_date, self.end_date)
         self.assertTrue(data.keys().__contains__('Open'))
         self.assertTrue(data.keys().__contains__('High'))
         self.assertTrue(data.keys().__contains__('Low'))
@@ -34,22 +21,9 @@ class TestYahooFinance(unittest.TestCase):
         self.assertTrue(data.keys().__contains__('Volume'))
 
     def test_get_pct_returns(self):
-        pct_returns = get_pct_returns(self.symbol, self.start_date, self.end_date)
+        pct_returns = yf.get_pct_returns(self.symbol, self.start_date, self.end_date)
         self.assertAlmostEqual(pct_returns[self.test_date], .0121811533129)
 
     def test_get_returns(self):
-        returns = get_returns(self.symbol, self.start_date, self.end_date)
+        returns = yf.get_returns(self.symbol, self.start_date, self.end_date)
         self.assertAlmostEqual(returns[self.test_date], 9.049988)
-
-    def test_get_current_price(self):
-        current_price = get_current_price(self.symbol)
-        self.assertTrue(type(current_price) is float)
-        self.assertGreaterEqual(current_price, 500)
-
-    def test_get_company_name(self):
-        company_name = get_company_name(self.symbol)
-        self.assertEqual(company_name, 'Google Inc.')
-
-    def test_get_company_sector(self):
-        company_sector = get_company_sector(self.symbol)
-        self.assertEqual(company_sector, 'Technology')
--- data/data_utils/yahoo_finance.py
+++ data/data_utils/yahoo_finance.py
@@ -1,39 +1,36 @@
-import os
-import ystockquote
-import pandas as pd
 import pandas_datareader.data as web
 from collections import OrderedDict
-from datetime import datetime as dt
 
-
-def get_stock_data(symbol, start_date=None, end_date=None):
+def get_stock_data(symbol, start_date, end_date):
     """
-    Get O/H/L/C stock data from Yahoo Finance for a single stock
+    Get daily resolution O/H/L/C stock data from Yahoo Finance for a single symbol.
     :param symbol: (string)
     :param start_date: (DateTime)
     :param end_date: (DateTime)
-    :return: (DataFrame) of stock data from start_date to end_date
-    """
-    if start_date is None:
-        start_date = dt(year=2016, month=1, day=1)
-
-    if end_date is None:
-        today = dt.today()
-        end_date = dt(year=today.year, month=today.month, day=today.day)
-
+    :return: (DataFrame)
+    |                     |   Open |    High |     Low |   Close |     Volume |   Adj Close |
+    |:--------------------|-------:|--------:|--------:|--------:|-----------:|------------:|
+    | 2016-01-04 00:00:00 | 743    | 744.06  | 731.258 |  741.84 | 3.2728e+06 |      741.84 |
+    | 2016-01-05 00:00:00 | 746.45 | 752     | 738.64  |  742.58 | 1.9507e+06 |      742.58 |
+    | 2016-01-06 00:00:00 | 730    | 747.18  | 728.92  |  743.62 | 1.947e+06  |      743.62 |
+    | 2016-01-07 00:00:00 | 730.31 | 738.5   | 719.06  |  726.39 | 2.9637e+06 |      726.39 |
+    | 2016-01-08 00:00:00 | 731.45 | 733.23  | 713     |  714.47 | 2.4509e+06 |      714.47 |
+    | 2016-01-11 00:00:00 | 716.61 | 718.855 | 703.54  |  716.03 | 2.0906e+06 |      716.03 |
+    | 2016-01-12 00:00:00 | 721.68 | 728.75  | 717.317 |  726.07 | 2.0245e+06 |      726.07 |
+    | 2016-01-13 00:00:00 | 730.85 | 734.74  | 698.61  |  700.56 | 2.5017e+06 |      700.56 |
+    | 2016-01-14 00:00:00 | 705.38 | 721.925 | 689.1   |  714.72 | 2.2258e+06 |      714.72 |
+    | 2016-01-15 00:00:00 | 692.29 | 706.74  | 685.37  |  694.45 | 3.5924e+06 |      694.45 |
+        """
     if start_date is not None and end_date is not None:
         assert start_date < end_date, "Start date is later than end date."
-
-    # log.info("Loading symbol: {}".format(symbol))
     symbol_data = web.DataReader(symbol, 'yahoo', start_date, end_date)
-
     return symbol_data
 
 
 def get_stock_data_multiple(symbols, start_date=None, end_date=None):
     """
-    Get OHLC stock data from Yahoo Finance for multiple stocks
-    :param symbols: (list) of symbols (string)
+    Get daily reoslution O/H/L/C stock data from Yahoo Finance for multiple symbols.
+    :param symbols: (list) of symbols (str)
     :param start_date: (DateTime)
     :param end_date: (DateTime)
     :return: (OrderedDict) of DataFrames of stock data from start_date to end_date
@@ -71,43 +68,3 @@ def get_returns(symbol, start_date=None, end_date=None, col='Adj Close'):
     """
     data = get_stock_data(symbol, start_date, end_date)[col]
     return data.diff().fillna(0)
-
-
-def get_current_price(symbol):
-    """
-    Get the latest price!
-    :param symbol:
-    :return:
-    """
-    quote = ystockquote.get_price(symbol)
-    if quote == 'N/A':
-        return None
-    return float(quote)
-
-
-def get_company_name(symbol):
-    """
-    Get the full name of the company by the symbol
-    :param symbol:
-    :return:
-    """
-    fpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'secwiki_tickers.csv')
-    df = pd.read_csv(fpath)
-    company_info = df[df.Ticker == symbol]
-    code = company_info['Name'].keys()[0]
-    company_name = company_info.to_dict()['Name'][code]
-    return company_name
-
-
-def get_company_sector(symbol):
-    """
-    Get the sector of the company
-    :param symbol: (str)
-    :return: (str)
-    """
-    fpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'secwiki_tickers.csv')
-    df = pd.read_csv(fpath)
-    company_info = df[df.Ticker == symbol]
-    code = company_info['Name'].keys()[0]
-    company_sector = company_info.to_dict()['Sector'][code]
-    return company_sector
